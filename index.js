17adaa05099e0acd417ce31919227a875e3cf016
baberarjumand <baber.arjumand@gmail.com>
2020-02-08 14:04:17 +0300

    completed exercise 4.5 part 2, mongoose odm p2

--- index.js
+++ index.js
@@ -32,17 +32,54 @@ connect.then((db) => {
     //         console.log(err);
     //     });
 
+    // Dishes.create({
+    //     name: 'Uthapizza',
+    //     description: 'Test'
+    // })
+    // .then((dish) => {
+    //     console.log(dish);
+        
+    //     return Dishes.find({}).exec();
+    // })
+    // .then((dishes) => {
+    //     console.log(dishes);
+
+    //     return Dishes.remove({});
+    // })
+    // .then(() => {
+    //     return mongoose.connection.close();
+    // })
+    // .catch((err) => {
+    //     console.log(err);
+    // });
+
     Dishes.create({
-        name: 'Uthapizza',
-        description: 'Test'
+        name: 'Uthappizza',
+        description: 'test'
     })
     .then((dish) => {
         console.log(dish);
-        
-        return Dishes.find({}).exec();
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
 
         return Dishes.remove({});
     })
